aa2da2bae67b434bfedc1e8a7d90d2890cf14dfa
Artem18rus <urart88@mail.ru>
2023-03-26 11:51:15 +0400

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+DZ8-2 useJsonFetch-front
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataComponent from "./components/DataComponent";
+import "./css/index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <DataComponent />
+    </div>
+  );
+}
+
+export default App;
